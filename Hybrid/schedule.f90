ca1e514a0e47da91682af1383d70e1dd20aff8ec
jjeongGrp <56035901+jjeongGrp@users.noreply.github.com>
2022-12-30 15:55:44 -0600

    Create schedule.f90

---
+++ Hybrid/schedule.f90
@@ -0,0 +1,19 @@
+program schedule
+	use omp_lib
+	implicit none
+	integer, parameter :: N = 100
+	integer :: i, tid, a(N)
+
+!$omp parallel private(i, tid)
+	tid = omp_get_thread_num()
+	if( tid /= 2 ) call sleep(2)
+	!$omp do schedule(dynamic, 5)
+	do i=0, N-1
+		a(i) = i
+		print *, 'a(', i, ')=', a(i), ' tid=', tid
+	end do
+	!$omp end do
+
+	print *, 'end ', tid, ' thread'
+!$omp end parallel
+end
