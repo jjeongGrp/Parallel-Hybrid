21a73deb884cc2e06f5ff821d94d2f681007bd68
jjeongGrp <56035901+jjeongGrp@users.noreply.github.com>
2022-12-30 11:59:48 -0600

    Create data_scope_shared.f90

---
+++ OpenMP/Basic/data_scope_shared.f90
@@ -0,0 +1,13 @@
+program data_scope_firstprivate
+    integer a(0:9), i, tid, omp_get_thread_num
+
+    call omp_set_num_threads(4)
+!$omp parallel shared(a) private(tid)
+    tid = omp_get_thread_num()
+    a(tid) = tid + 1
+!$omp end parallel
+
+    do i=0, 3
+        print *, 'a(', i, ') = ', a(i)
+    end do
+end
