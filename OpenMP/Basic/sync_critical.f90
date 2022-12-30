28d166b28ad93d93c84b622147027053e595abaa
jjeongGrp <56035901+jjeongGrp@users.noreply.github.com>
2022-12-30 12:05:22 -0600

    Create sync_critical.f90

---
+++ OpenMP/Basic/sync_critical.f90
@@ -0,0 +1,23 @@
+program sync_critical
+    integer, parameter :: N=20
+
+    integer :: i, sum=0, omp_get_thread_num
+    integer, dimension(0:N-1) :: A, B
+
+    do i=0, N-1
+        A(i) = i+1
+        B(i) = i+2
+    end do
+
+    call omp_set_num_threads(4)
+!$omp parallel
+    !$omp do
+    do i=0, N-1
+        !$omp critical
+        sum = sum + A(i) * B(i)
+        !$omp end critical
+    end do
+!$omp end parallel
+
+    print *, 'sum =', sum
+end
