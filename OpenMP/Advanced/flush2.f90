94ca72e869ccf302e0defb10879fd8599b06e456
jjeongGrp <56035901+jjeongGrp@users.noreply.github.com>
2022-12-30 12:28:35 -0600

    Create flush2.f90

---
+++ OpenMP/Advanced/flush2.f90
@@ -0,0 +1,18 @@
+program flush2
+    integer :: x, tid, omp_get_thread_num
+
+    call omp_set_num_threads(4)
+!$omp parallel private(tid)
+    x = 0
+    tid = omp_get_thread_num()
+    if( tid == 0 ) then
+        call sleep(3)
+        x = 1
+        call sleep(3)
+    end if
+    do while( x == 0 )
+        !$omp flush(x)
+    end do
+    print *, 'x=',x, ' in ', tid, '-th thread'
+!$omp end parallel
+end
