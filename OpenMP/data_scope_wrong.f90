2c590b2dc208b95c464f5d0d454951368318bc4e
jjeongGrp <56035901+jjeongGrp@users.noreply.github.com>
2022-12-30 11:51:44 -0600

    Delete data_scope_wrong.f90

--- OpenMP/data_scope_wrong.f90
+++
@@ -1,9 +0,0 @@
-program hello_wrong
-    integer tid, omp_get_thread_num
-
-    call omp_set_num_threads(4)
-!$omp parallel
-    tid = omp_get_thread_num()
-    print *, 'I am ', omp_get_thread_num(), ' tid = ', tid
-!$omp end parallel
-end
