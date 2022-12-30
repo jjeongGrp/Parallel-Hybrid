dcf6a236833c1a916eecbf038d9bc7a51a7972b4
jjeongGrp <56035901+jjeongGrp@users.noreply.github.com>
2022-12-30 12:14:06 -0600

    Create nested_parallel_for.f90

---
+++ OpenMP/Basic/nested_parallel_for.f90
@@ -0,0 +1,16 @@
+program nested_parallel_do
+    integer, parameter :: N=20
+
+    integer :: tid, i, omp_get_thread_num
+
+    call omp_set_num_threads(4)
+!$omp parallel private(tid)
+    tid = omp_get_thread_num()
+
+    !$omp parallel do
+    do i=0, N-1
+        print *, 'Hello World', tid, i
+    end do
+    !$omp end parallel do
+!$omp end parallel
+end
